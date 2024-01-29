b7272513b6202695f9fbb1946447d9c10426a3e6
KkarliskK <karlisbirk@gmail.com>
2024-01-29 20:19:39 +0200

    register works now

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -18,6 +18,7 @@ class AuthController extends Controller
             'name' => 'required|string',
             'username' => 'required|string',
             'email' => 'required|string',
+            'password' => 'required|string',
         ]);
         if ($validator->fails()){
             return response()->json([
